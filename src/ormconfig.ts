d5155668a0bbdf7b612d66ff10630a744b2435f2
Teguh Willyanto <willyantoteguh1@gmail.com>
2021-08-22 13:45:07 +0700

    Update Credentials

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -11,12 +11,21 @@ const config = {
 
 const connectionOptions: ConnectionOptions = {
     type: "postgres",
-    host: "localhost",
+    host: "ec2-34-233-114-40.compute-1.amazonaws.com",
     port: 5432,
-    username: config.user,
-    password: config.password,
-    database: config.database || "postgres",
+    url: "postgres://zputfhlukuvbkm:c73dad7eb14e3d89d4b303c6dde3ce5c3553ffbcbc857f4d8ac51983d01e97bd@ec2-34-233-114-40.compute-1.amazonaws.com:5432/d2h1q4vm4m1pbf",
+    // username: config.user,
+    // password: config.password,
+    // database: config.database || "postgres",
+    username: "zputfhlukuvbkm",
+    password: "c73dad7eb14e3d89d4b303c6dde3ce5c3553ffbcbc857f4d8ac51983d01e97bd",
+    database: "d2h1q4vm4m1pbf",
     entities: [User],
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
     synchronize: true,
     dropSchema: false,
     migrationsRun: true,
